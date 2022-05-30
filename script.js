a34097000116b3aabde017eaf85d69d5684fd475
SamuelLorrain <samuel.lorrain@gmail.com>
2022-05-30 19:46:22 +0200

    add controls

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.body.onload = draw;
 
 const canvas = document.querySelector("canvas");
+const playBtn = document.querySelector(".play");
+playBtn.onclick = start;
+const pauseBtn = document.querySelector(".pause");
+pauseBtn.onclick = stop;
+const checkerBoardBtn = document.querySelector(".checker");
+checkerBoardBtn.onclick = checkerBoard;
 const ctx = canvas.getContext("2d");
-const xElements = 16;
-const yElements = 16;
-const size = 50;
+const xElements = 32;
+const yElements = 32;
+const size = 25;
 
 const colors = {
     0: '#fff',
@@ -12,23 +18,35 @@ const colors = {
 }
 let map = new Uint8Array(xElements*yElements);
 let newMap = new Uint8Array(xElements*yElements);
-// for(let i = 0; i < xElements; i++) {
-//     for(let j = 0; j < yElements; j++) {
-//         if((i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1)) {
-//             map[j*xElements+i] = 1;
-//         }
-//     }
-// }
+let intervalHandler = undefined;
+
+function start() {
+    if(!intervalHandler) {
+        intervalHandler = setInterval(updateCells, 500);
+    }
+}
+function stop() {
+    clearInterval(intervalHandler);
+}
+function checkerBoard() {
+    for(let i = 0; i < xElements; i++) {
+        for(let j = 0; j < yElements; j++) {
+            if((i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1)) {
+                map[j*xElements+i] = 1;
+            }
+        }
+    }
+    draw();
+}
 
 canvas.onmousedown = onMouseDown;
-canvas.width = 16*size;
-canvas.height = 16*size;
+canvas.width = xElements*size;
+canvas.height = yElements*size;
 
 function onMouseDown(e) {
     let canvasX = Math.floor(e.offsetX/size);
     let canvasY = Math.floor(e.offsetY/size);
     map[canvasY*xElements+canvasX] = map[canvasY*xElements+canvasX] == 1 ? 0 : 1;
-    console.log(canvasX, canvasY, canvasY*xElements+canvasX);
     draw();
 }
 
@@ -36,7 +54,12 @@ function draw() {
     for(let j = 0; j < yElements; j++) {
         for(let i = 0; i < xElements; i++) {
             ctx.fillStyle = colors[map[j*xElements+i]];
-            ctx.fillRect(i*size,j*size,size,size);
+            ctx.fillRect(
+                i*size,
+                j*size,
+                size,
+                size
+            );
         }
     }
 }
@@ -59,46 +82,14 @@ function indexFromXY(a) {
 function adjacents(index) {
     let [x,y] = xyFromIndex(index);
     return {
-        left: {
-            x:x-1,
-            y:y,
-            index: indexFromXY([x-1, y]),
-        },
-        right: {
-            x:x+1,
-            y:y,
-            index: indexFromXY([x+1, y])
-        },
-        up: {
-            x:x,
-            y:y-1,
-            index: indexFromXY([x, y-1]),
-        },
-        down: {
-            x:x,
-            y:y+1,
-            index: indexFromXY([x, y+1])
-        },
-        upLeft: {
-            x:x-1,
-            y:y-1,
-            index: indexFromXY([x-1, y-1])
-        },
-        upRight: {
-            x:x+1,
-            y:y-1,
-            index: indexFromXY([x+1, y-1])
-        },
-        downLeft: {
-            x:x-1,
-            y:y+1,
-            index: indexFromXY([x-1, y+1])
-        },
-        downRight: {
-            x:x+1,
-            y:y+1,
-            index: indexFromXY([x+1, y+1])
-        },
+        left: indexFromXY([x-1, y]),
+        right: indexFromXY([x+1, y]),
+        up: indexFromXY([x, y-1]),
+        down: indexFromXY([x, y+1]),
+        upLeft: indexFromXY([x-1, y-1]),
+        upRight: indexFromXY([x+1, y-1]),
+        downLeft: indexFromXY([x-1, y+1]),
+        downRight: indexFromXY([x+1, y+1]),
     };
 }
 
@@ -110,15 +101,14 @@ function aliveInNextGen(index) {
     // of the neighbour is outside of the map,
     // it is equal to 0
     let aliveNeighbours = 0;
-
-    aliveNeighbours += isLegit(neighbours.left.index) ? map[neighbours.left.index] : 0;
-    aliveNeighbours += isLegit(neighbours.right.index) ? map[neighbours.right.index] : 0;
-    aliveNeighbours += isLegit(neighbours.up.index) ? map[neighbours.up.index] : 0;
-    aliveNeighbours += isLegit(neighbours.down.index) ? map[neighbours.down.index] : 0;
-    aliveNeighbours += isLegit(neighbours.upLeft.index) ? map[neighbours.upLeft.index] : 0;
-    aliveNeighbours += isLegit(neighbours.upRight.index) ? map[neighbours.upRight.index] : 0;
-    aliveNeighbours += isLegit(neighbours.downLeft.index) ? map[neighbours.downLeft.index] : 0;
-    aliveNeighbours += isLegit(neighbours.downRight.index) ? map[neighbours.downRight.index] : 0;
+    aliveNeighbours += isLegit(neighbours.left) ? map[neighbours.left] : 0;
+    aliveNeighbours += isLegit(neighbours.right) ? map[neighbours.right] : 0;
+    aliveNeighbours += isLegit(neighbours.up) ? map[neighbours.up] : 0;
+    aliveNeighbours += isLegit(neighbours.down) ? map[neighbours.down] : 0;
+    aliveNeighbours += isLegit(neighbours.upLeft) ? map[neighbours.upLeft] : 0;
+    aliveNeighbours += isLegit(neighbours.upRight) ? map[neighbours.upRight] : 0;
+    aliveNeighbours += isLegit(neighbours.downLeft) ? map[neighbours.downLeft] : 0;
+    aliveNeighbours += isLegit(neighbours.downRight) ? map[neighbours.downRight] : 0;
 
     if (current) {
         if (aliveNeighbours == 2 || aliveNeighbours == 3) {
@@ -135,7 +125,9 @@ function aliveInNextGen(index) {
 }
 
 function isLegit(index) {
-    return index >= 0 && index < xElements*yElements;
+    let [x,y] = xyFromIndex(index);
+    isInRange = !!(index >= 0 && index < xElements*yElements);
+    return isInRange && x >= 0 && x < xElements-1 && y >= 0 && y < yElements-1;
 }
 
 function updateCells() {
